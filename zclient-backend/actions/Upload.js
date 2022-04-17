aa6e850086457d3340e28c60d4dd89992840dd90
hieu <htrinhtrieu@pentalog.com>
2022-04-17 21:03:54 +0700

    285. Upload a file through REST API - part 3

--- zclient-backend/actions/Upload.js
+++ zclient-backend/actions/Upload.js
@@ -1,4 +1,5 @@
 import {Login} from './Login.js';
+import {baseURL} from '../src/config.js';
 
 export class Upload extends Login  {
 
@@ -11,8 +12,26 @@ export class Upload extends Login  {
 
     Upload()
     {
-        console.log(this.files, this.product_id)
+        let data = new FormData()
+        data.append('imageFile', this.files[0], this.files[0].name)
+        data.append('product_id', this.product_id)
 
+        let config = {
+            headers: {
+                'content-type': 'multipart/form-data'
+            }
+        }
+
+        axios.post(baseURL+'/api/upload', data, config)
+            .then((response) => {
+                console.log(response);
+            }).catch((error) => {
+            console.log(error);
+            if( error.response.data.code == "401" )
+            {
+                this.handle401Error()
+            }
+        })
     }
 
 }
